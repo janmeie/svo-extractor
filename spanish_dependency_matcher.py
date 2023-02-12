a758aaa1284cd000ab77fc2cac3cf32ec65d4ece
janameier <jana.meier@uzh.ch>
2023-02-12 22:39:40 +0100

    update spanish_dependency_matcher.py

--- spanish_dependency_matcher.py
+++ spanish_dependency_matcher.py
@@ -7,7 +7,7 @@ from spacy.matcher import DependencyMatcher
 hits = {}
 
 
-def get_extension(doc, head_id, dependency):
+def get_extension(doc, head_id):
     """Gets compound words.
 
     All subjects and objects are checked for compound words such as names (e.g. Lisa Maria Müller). The input is the
@@ -15,19 +15,19 @@ def get_extension(doc, head_id, dependency):
     the found compound together with their head or simply None if nothing is found.
 
     :param doc: the entire text to be analysed
-    :param head_id: either the noun (subj or obj) which the compound word depends on
+    :param head_id: the noun (subj or obj) which the compound word depends on
 
     :input: 1
     :return: [0, 1]
     """
-    compound_ids = [c.i for c in doc[head_id].children if c.dep_ == dependency]
+    compound_ids = [c.i for c in doc[head_id].children if c.dep_ == "flat"]
     if compound_ids:
         return sorted(compound_ids + [head_id])
     else:
         return None
 
 
-def get_compounds(nlp, doc, match_id, token_ids, svo_subject, subj_idx, svo_object, obj_idx):
+def get_compounds(doc, token_ids, subj_idx, obj_idx):
     """Checks if the matched pattern is already in the dictionary. If this is not the case
     the function returns the matched pattern together with the noun compounds.
 
@@ -41,12 +41,10 @@ def get_compounds(nlp, doc, match_id, token_ids, svo_subject, subj_idx, svo_obje
     :return: (2, 1, 4) [0, 1] None
     """
     ids = tuple(token_ids)
-    compound_subj = get_extension(doc, ids[subj_idx], "flat")
-    compound_obj = get_extension(doc, ids[obj_idx], "flat")
+    compound_subj = get_extension(doc, ids[subj_idx])
+    compound_obj = get_extension(doc, ids[obj_idx])
 
     if not ids in hits:
-        # print(nlp.vocab[match_id].text, '\n', doc[svo_subject].text, compound_subj,
-        #      doc[svo_object].text, compound_obj)
         return ids, compound_subj, compound_obj
 
 
@@ -80,39 +78,6 @@ def update_svo_triplets_from_sentence(nlp, doc):
         }
     ]
 
-    pattern_passive = [
-        {
-            "RIGHT_ID": "passive_verb",
-            "RIGHT_ATTRS": {"POS": "VERB"}
-        },
-        {
-            "LEFT_ID": "passive_verb",
-            "REL_OP": ">",
-            "RIGHT_ID": "passive_subject",
-            "RIGHT_ATTRS": {"DEP": "nsubj"}
-        },
-        {
-            "LEFT_ID": "passive_verb",
-            "REL_OP": ">",
-            "RIGHT_ID": "passive_obj",
-            "RIGHT_ATTRS": {"DEP": "obl"}
-        },
-        {
-            "LEFT_ID": "passive_verb",
-            "REL_OP": ">",
-            "RIGHT_ID": "passive_aux",
-            "RIGHT_ATTRS": {"LEMMA": "ser"}
-        },
-        {
-            "LEFT_ID": "passive_obj",
-            "REL_OP": ">",
-            "RIGHT_ID": "passive_prep",
-            "RIGHT_ATTRS": {"TEXT": "por"}
-        }
-
-
-    ]
-
     def svo_match(matcher, doc, id, matches):
         """Callback function to act on matches. In this case on SVO matches.
 
@@ -142,7 +107,7 @@ def update_svo_triplets_from_sentence(nlp, doc):
 
         if len(adposition) == 0 or a_preposition:
             # get compound nouns
-            ids, compound_subj, compound_obj = get_compounds(nlp, doc, match_id, token_ids, svo_subject, 1, svo_object, 2)
+            ids, compound_subj, compound_obj = get_compounds(doc, token_ids, 1, 2)
 
             # add pattern name, S, V, O to the dictionary
             hits[ids] = {'pattern_name': nlp.vocab[match_id].text, 'S': ids[1], 'V': ids[0], 'O': ids[2]}
@@ -153,35 +118,7 @@ def update_svo_triplets_from_sentence(nlp, doc):
             if compound_obj:
                 hits[ids]['CO'] = compound_obj
 
-    def passive_match(matcher, doc, id, matches):
-        """Callback function to act on matches. In this case on passive matches.
-
-        :param matcher: Dependency Matcher.
-        :param doc: The entire document to be analysed.
-        :param id: The ID of the particular pattern.
-        :param matches: the matches of the specified patterns which consist of a list containing (match_id, token_ids)
-        tuples
-        """
-        single_match = matches[id]
-        match_id, token_ids = single_match
-        svo_verb, svo_object, svo_subject, svo_aux, svo_prep = token_ids
-
-        ids, compound_subj, compound_obj = get_compounds(nlp, doc, match_id, token_ids, svo_subject, 2, svo_object, 1)
-
-        # get rid of the index of the preposition 'por', only index of S, V and O are relevant.
-        id = ids[:3]
-
-        hits[id] = {'pattern_name': nlp.vocab[match_id].text, 'S': ids[2], 'V': ids[0], 'O': ids[1]}
-
-        if compound_subj:
-            hits[id]['CS'] = compound_subj
-        if compound_obj:
-            hits[id]['CO'] = compound_obj
-
     matcher.add("SVO", [pattern_svo], on_match=svo_match)
-    matcher.add("passive", [pattern_passive], on_match=passive_match)
-
     matcher(doc)
 
-    print(hits)
     return hits
